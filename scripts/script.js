c01f2eb34f88bbf6c8971e01daa4821bc06dd296
Daniel Lazzaroni <dlazzaroni@my.bcit.ca>
2022-11-22 16:22:15 -0800

    colors

--- scripts/script.js
+++ scripts/script.js
@@ -28,9 +28,28 @@ clickButton.addEventListener('click', clicked)
 const image = document.querySelector('#shoppingCart')
 function updateImage(){
     image.src="Images/cart.png";
+    image.alt="shopping cart";
     image.width="100";
     image.height="100";
     // image.setAttribute('src',"Images/cart.png")
 }
 
 clickButton.addEventListener('click', updateImage)
+
+
+const buttonContainer = document.querySelector('.button-container')
+
+
+function changeBGGreen(e){
+    if (e.target.tagName === 'BUTTON'){
+        e.target.classList.add("greenBG")
+    }
+}
+
+buttonContainer.addEventListener('clicked', changeColour)
+function changeColour(e){
+    e.target.style.color = e.target.textContent
+}
+
+
+
